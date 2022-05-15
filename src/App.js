d41885a1977dbb1589fbfbe02baf2d643d83e8c3
chin hui <ekchinhui@gmail.com>
2022-05-16 01:10:33 +0800

    Remove unused components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import { Navbar, Container, Nav, Button } from 'react-bootstrap';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 
 function App() {
   return (
