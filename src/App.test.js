c3f050e62cb8ad644e50036ab2a2b38ed6ee6310
chin hui <ekchinhui@gmail.com>
2022-05-16 00:59:49 +0800

    Update tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+test('render instructions link', () => {
+  render(<App />);
+  expect(screen.getByText(/Instructions/)).toBeInTheDocument();
+})
+
+test('render single player link', () => {
+  render(<App />);
+  expect(screen.getByText(/Single Player/)).toBeInTheDocument();
+})
